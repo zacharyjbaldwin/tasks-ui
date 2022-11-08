a4f3ad1198698e713799cc286a9d249ee0d8eca4
Zachary Baldwin <zacharyjbaldwin@gmail.com>
2022-11-07 20:17:28 -0600

    added expand/collapse all buttons and changed alignment of some buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { Day } from './shared/day.enum';
+import { TaskGroupComponent } from './task-group/task-group.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChildren(TaskGroupComponent) taskGroups?: QueryList<TaskGroupComponent>;
+
   public today = new Date().getDay() - 1;
   public days: Day[] = [Day.Monday, Day.Tuesday, Day.Wednesday, Day.Thursday, Day.Friday, Day.Saturday, Day.Sunday];
+  public displayDays: Day[] = [];
+
+  ngOnInit(): void {
+    for (let i = this.today; i < this.today + 7; i++ ) {
+      this.displayDays.push(this.days[i % 7]);
+    }
+  }
+
+  public expandAll(): void {
+    this.taskGroups?.forEach(taskGroup => taskGroup.hideContent = false);
+  }
+  
+  public collapseAll(): void {
+    this.taskGroups?.forEach(taskGroup => taskGroup.hideContent = true);
+  }
 }
