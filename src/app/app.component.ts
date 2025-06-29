8d7dd63e038f63c7bd9f11b527c4a3942dd68b01
Zachary Baldwin <zacharyjbaldwin@gmail.com>
2025-06-29 12:27:21 -0500

    updated page layout on xl screens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,48 @@
-import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  inject,
+  OnInit,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
 import { Day } from './shared/day.enum';
 import { TaskGroupComponent } from './task-group/task-group.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   @ViewChildren(TaskGroupComponent) taskGroups?: QueryList<TaskGroupComponent>;
 
-  public days: Day[] = [Day.Monday, Day.Tuesday, Day.Wednesday, Day.Thursday, Day.Friday, Day.Saturday, Day.Sunday];
+  public days: Day[] = [
+    Day.Monday,
+    Day.Tuesday,
+    Day.Wednesday,
+    Day.Thursday,
+    Day.Friday,
+    Day.Saturday,
+    Day.Sunday,
+  ];
   public displayDays: Day[] = [];
 
   get today() {
     let date = new Date().getDay() - 1;
-    return date < 0 ? date += 7 : date;
+    return date < 0 ? (date += 7) : date;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onScreenResize(event: Event) {
+    let screenWidth = (event.target as Window).innerWidth;
+    if (screenWidth > 900) {
+      this.expandAll();
+    }
   }
 
   refreshAllNoSpinner(): void {
-    this.taskGroups?.forEach(taskGroup => taskGroup.getTasks());
+    this.taskGroups?.forEach((taskGroup) => taskGroup.getTasks());
   }
 
   ngOnInit(): void {
@@ -29,10 +52,10 @@ export class AppComponent implements OnInit {
   }
 
   public expandAll(): void {
-    this.taskGroups?.forEach(taskGroup => taskGroup.hideContent = false);
+    this.taskGroups?.forEach((taskGroup) => (taskGroup.hideContent = false));
   }
 
   public collapseAll(): void {
-    this.taskGroups?.forEach(taskGroup => taskGroup.hideContent = true);
+    this.taskGroups?.forEach((taskGroup) => (taskGroup.hideContent = true));
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000'
+  apiUrl: 'http://localhost:3000',
+  // apiUrl: 'https://tasks-api-zacharyjbaldwin-com.herokuapp.com',
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
